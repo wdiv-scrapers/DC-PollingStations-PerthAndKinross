a856e44916294f981086b339fdfc355aea1a400d
chris48s <chris.shaw480@gmail.com>
2018-09-26 09:38:25 +0100

    change search URL

--- scraper.py
+++ scraper.py
@@ -2,7 +2,7 @@ from dc_base_scrapers.geojson_scraper import GeoJsonScraper
 from dc_base_scrapers.hashonly_scraper import HashOnlyScraper
 
 
-search_url = "http://gis-pkc.opendata.arcgis.com/datasets.json?q=polling&sort_by=-updatedAt"
+search_url = "https://opendata.arcgis.com/api/v2/datasets?filter%5Bcatalogs%5D=gis-pkc.opendata.arcgis.com&include=organizations%2Cgroups&page%5Bnumber%5D=1&page%5Bsize%5D=10&q=polling&sort=-updatedAt"
 stations_url = "http://gis-pkc.opendata.arcgis.com/datasets/909e45688aa646199cad8e8616ddef7a_0.geojson"
 districts_url = "http://gis-pkc.opendata.arcgis.com/datasets/6dac129ea3cb492b961186e316e09690_0.geojson"
 council_id = 'S12000024'
